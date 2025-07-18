129100875521b86ac2b58dcd40516ea92b55b033
Karan Kumar <karankannaujiya129@gmail.com>
2025-07-18 17:19:15 +0530

    Removed nested git and added full frontend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000; // Use port from .env or default to 5000
 
 // Middleware
-app.use(cors()); // Enable CORS for all routes (important for React frontend)
+const cors = require("cors");
+
+app.use(cors({
+  origin: ["http://nitishbhardwaj.site", "https://nitishbhardwaj.site"],
+  credentials: true
+}));
+
 app.use(express.json()); // Body parser for JSON requests
 
 // MongoDB Connection
